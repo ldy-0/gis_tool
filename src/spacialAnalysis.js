f468bb1f2fab2d6a11c71db2b211c1e4bb947a80
person-0 <13213891450@163.com>
2018-05-16 22:26:14 +0800

    swm

--- src/spacialAnalysis.js
+++ src/spacialAnalysis.js
@@ -5,10 +5,47 @@
  
  'use strict'
  
+/**
+ *
+ * Create spacial weight matrix / 创建空间权重矩阵
+ *
+ * @param {String} input  file path
+ * @param {String} output file path
+ */
+function SWM(input, output){
+	let arr = [],
+		weight_arr,
+		ws = fs.createWriteStream(output);
+    
+	readRow(input, row => {
+    
+		if(row.match(/lon/)){
+			return ;
+		}
+    
+		arr.push(row.split(',').slice(0,3));
+    
+	}, ()=>{
+    
+		weight_arr = arr.map((val, index, a)=>{
+			let p1 = unit.DtoM([Number(val[1]), Number(val[0])]);
+      
+			let w_arr = arr.map(function(v){
+				let p2 = unit.DtoM([Number(v[1]), Number(v[0])]);
+        
+				return Number(Math.sqrt((p2[0]-p1[0])**2+(p2[1]-p1[1])**2).toFixed(0)) < 200000 ? 1 : 0 ;
+			});
+      
+			ws.write(w_arr+os.EOL);
+		});
+
+	});
+}
  
  /**
-  * Compute global Moran's
-  * 全局莫兰指数
+  *
+  * Compute global Moran's I / 全局莫兰指数
+  *
   * @param {Array} data_arr 样本数组
   * @param {Array} weight_matrix 权重矩阵
   * @return {Number}
